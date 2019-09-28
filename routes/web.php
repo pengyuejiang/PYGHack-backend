05eae9ce4340e573c22f88d0e2207458b8ca524a
Zimo Xiao <xiaozimo@zuggr.com>
2019-09-28 15:21:59 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -57,7 +57,3 @@ $router->group(['prefix' => 'survey', 'middleware' => ['auth']], function ($rout
         $router->get('', 'SurveyTemplateController@index');
     });
 });
-
-$router->get('test', function () {
-    return $request->session()->get('token');
-});
